0a47d5ed5565cb6eceb40be85fc66b76a79501e2
Emily <emily.j.langley@gmail.com>
2024-03-27 16:57:48 -0700

    added create movie rating route

--- server.py
+++ server.py
@@ -64,6 +64,7 @@ def process_login():
 
     return redirect("/")
 
+
 @app.route("/movies/<movie_id>")
 def show_movie(movie_id):
     """Show details on a particular movie."""
@@ -72,6 +73,29 @@ def show_movie(movie_id):
 
     return render_template("movie_details.html", movie=movie)
 
+
+@app.route("/movies/<movie_id>/ratings", methods=["POST"])
+def create_rating(movie_id):
+    """Create a new rating for the movie."""
+
+    logged_in_email = session.get("user_email")
+    rating_score = request.form.get("rating")
+
+    if logged_in_email is None:
+        flash("You must log in to rate a movie.")
+    elif not rating_score:
+        flash("Error: you didn't select a score for your rating.")
+    else:
+        user = crud.get_user_by_email(logged_in_email)
+        movie = crud.get_movie_by_id(movie_id)
+
+        rating = crud.create_rating(user, movie, int(rating_score))
+        db.session.add(rating)
+        db.session.commit()
+
+        flash(f"You rated this movie {rating_score} out of 5.")
+
+    return redirect(f"/movies/{movie_id}")
 
 if __name__ == "__main__":
     connect_to_db(app)
