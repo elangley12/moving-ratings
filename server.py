ff72b5d859448abf08bed7d15f9a42a2c8e52461
Emily <emily.j.langley@gmail.com>
2024-03-27 16:11:56 -0700

    initial commit

--- server.py
+++ server.py
@@ -7,9 +7,17 @@ from model import connect_to_db, db
 import crud
 from jinja2 import StrictUndefined
 
+
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def homepage():
+    """View homepage."""
+
+    return render_template('homepage.html')
 
 
 if __name__ == "__main__":
