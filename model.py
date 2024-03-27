034c002484f7ade642713cc8a9ffa5e1bf9744d4
Emily <emily.j.langley@gmail.com>
2024-03-27 12:34:59 -0700

    added rating relationship to User class

--- model.py
+++ model.py
@@ -46,6 +46,8 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
 
+    ratings = db.relationship("Rating", back_populates="user")
+
     def __repr__(self):
         return f"<User user_id={self.user_id} email={self.email}>"
 
